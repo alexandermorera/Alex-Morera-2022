66af84a0364c25242cc4367a86bab9e13c2b91ea
alexandermorera <81329278+alexandermorera@users.noreply.github.com>
2021-04-11 22:56:00 -0500

    Add files via upload

--- web.php
+++ web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\EmpleadosController;
+use App\Models\Empleado;
 use Illuminate\Support\Facades\Route;
 use App\Models\Empresa;
+
 
 
 /*
@@ -15,11 +18,37 @@ use App\Models\Empresa;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+/* Route::get('/', function () {
+    return view('pages.home');
 });
 
-Route::get('testEmpresas', function(){
-	$empresa = Empresa::where('nit','854335')->first();
-	dd($empresa);	
+Route::get('/contact', function () {
+    return view('pages.contact');
 });
+
+Route::get('testEmpresas',function() {
+
+	$empleados = Empleado::where('nombre','like','a%')->orderBy('nombre')->get();
+	dd($empleados);
+
+});
+
+Route::get('/empleado', function (){
+	$empleados = Empleado::all();
+	dd($empleados);
+
+});
+
+Route::get('saludar/{idEmpleado}', function($idEmpleado){
+	$empleado = Empleado::where('id', $idEmpleado)->first();
+	return view('hola')->with('empleado',$empleado);
+});
+
+Route::get('saludoEmpleados/{estado}', function($estado){
+	$empleados = Empleado::where('estado',$estado)->get();
+	return view('hola',compact('empleados','estado'));
+}); 
+ */
+Route::resource('empleados',EmpleadosController::class);
